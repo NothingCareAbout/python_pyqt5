5bf1e4108718c71e87926ef09dcd199641bdf116
zhouwulin <78009100+zhouwulin@users.noreply.github.com>
2022-03-19 00:22:47 +0800

    菜单栏学习
    通过代码实现菜单的创建，action的添加等

--- menubar.py
+++ menubar.py
@@ -1,4 +1,5 @@
 
+from re import S
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon
@@ -31,17 +32,28 @@ class Example(QMainWindow):
         exitAct.setStatusTip('Exit application')
         exitAct.triggered.connect(QApplication.instance().quit)
         #再来一个状态栏
-        openfileAct = QAction(QIcon('exit.png'), '&OpenFile', self)#添加Action
+        openfileAct = QAction(QIcon('exit.png'), '&OpenFile', self)
         openfileAct.setShortcut('Ctrl+O')
         openfileAct.setStatusTip('OpenFile')
         openfileAct.triggered.connect(self.openFile)
-        self.statusBar()
+        
+        #再来一个勾选菜单
+        viewstatAct = QAction('Viewstatbar',self,checkable=True)
+        viewstatAct.setStatusTip('view statbar')
+        viewstatAct.setChecked(True)
+        viewstatAct.triggered.connect(self.toggleMenu)
+        #实例化
+        self.statusbar=self.statusBar()
+        self.statusbar.showMessage("ready")
+        
 
         menubar = self.menuBar()
         fileMenu = menubar.addMenu('&File')
         openMenu = menubar.addMenu('&Edit')
+        viewMenu=menubar.addMenu("&View")
         fileMenu.addAction(exitAct)
         openMenu.addAction(openfileAct)
+        viewMenu.addAction(viewstatAct)
 
         self.resize(800,600)
         self.move(1200,800)
@@ -53,7 +65,46 @@ class Example(QMainWindow):
         self.plainText.resize(300,600)
         self.plainText.move(300,100)
         
+        #子菜单 submenu
+        impMenu = QMenu('Import', self)
+        impAct = QAction('Import mail', self)
+        impMenu.addAction(impAct)
+
+        newAct = QAction('New', self)
+
+        fileMenu.addAction(newAct)
+        fileMenu.addMenu(impMenu)
+        
         self.show()
+        
+        
+        #上下文菜单
+        #上下文菜单，也称为弹出菜单，是在某些上下文下出现的命令列表。
+        # 例如，在 Opera 浏览器中，在网页上按下鼠标右键，我们会得到一个上下文菜单，
+        # 这个菜单上，可以重新加载页面、返回或查看页面源代码。
+        # 如果右击工具栏，会得到另一个用于管理工具栏的上下文菜单。
+        
+    def contextMenuEvent(self, event):
+
+        cmenu = QMenu(self)
+
+        newAct = cmenu.addAction("New")
+        openAct = cmenu.addAction("打开文件")
+        quitAct = cmenu.addAction("Quit")
+        action = cmenu.exec(self.mapToGlobal(event.pos()))  
+        #右键工具栏，出现三个以上提到的菜单,点击跳转到相应的事件响应
+        #使用 exec 方法调出上下文菜单，
+        # 通过鼠标事件对象获得鼠标坐标点，再调用 
+        # mapToGlobal 方法把组件的坐标设置成全局的屏幕坐标。
+        if action == quitAct:
+            QApplication.instance().quit()
+            
+        elif action==newAct:
+            self.plainText.setPlainText("这是一个新见文件的右键菜单")
+        
+        elif action==openAct:
+            self.openFile()
+            
         
     ''' QFileDialog.getOpenFileName()    #获取一个打开文件的文件名
         QFileDialog.getOpenFileNames()   #获取多个打开文件的文件名
@@ -61,14 +112,22 @@ class Example(QMainWindow):
         QFileDialog.getOpenFileUrls()    #获取多个打开文件的统一资源定位符
         QFileDialog.getSaveFileName()    #获取保存的文件名
         QFileDialog.getSaveFileUrl()     #获取保存的url
-    '''   
+    ''' 
+    
+    def toggleMenu(self, state):
+    
+        if state:
+            self.statusbar.show()  #如果可见，则菜单栏是勾选状态
+        else:
+            self.statusbar.hide()
+              
     def openFile(self): #注意是不同文件之间是双分号
         dirpath=QFileDialog.getOpenFileName(self,"请选择文件",'',"txtFile(*.txt);;Excel files(*.xls xlxs)")
         #QFileDialog.getExistingDirectory(self,"请选择目录") #返回的是tuple
-        filepath=dirpath[0]
-        with open(filepath,'r',encoding='utf-8') as f:
-            text=f.read()
-            self.plainText.setPlainText(text)
+        #filepath=dirpath[0] #返回的是元祖，再操作
+            #with open(filepath,'r',encoding='utf-8') as f:
+            #text=f.read()
+           # self.plainText.setPlainText(text)
             
             
             
